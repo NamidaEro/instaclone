73fe41975a39fd9b45add6ff1c034c44fbc51999
진욱 <rkseksgkrns@kakao.com>
2021-08-28 07:13:03 +0000

    c

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -16,120 +16,129 @@ function Login(props) {
                     <Col md="auto login_header"></Col>
                 </Row>
             </Container>
+            <Container className="justify-content-lg-center">
+                <Row className="justify-content-lg-center container-min-width-30">
+                    <Col sm={{ span: 3 }} className="login_leftside phone_body text-center">
+                        <Row className="phone_header"></Row>
+                        <Row>
+                            <Col className="phone_body_img">
+                                <Carousel>
+                                    <Carousel.Item>
+                                        <img
+                                            className="phone_body_img_size"
+                                            src="https://www.instagram.com/static/images/homepage/screenshot1.jpg/d6bf0c928b5a.jpg"
+                                            alt="first"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="phone_body_img_size"
+                                            src="https://www.instagram.com/static/images/homepage/screenshot2.jpg/6f03eb85463c.jpg"
+                                            alt="second"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="phone_body_img_size"
+                                            src="https://www.instagram.com/static/images/homepage/screenshot3.jpg/f0c687aa6ec2.jpg"
+                                            alt="third"
+                                        />
+                                    </Carousel.Item>
+                                </Carousel>
+                            </Col>
 
-            <Row className="justify-content-md-center container-min-width-30">
-                <Col sm={{ span: 3 }} className="login_leftside phone_body">
-                    <Row className="phone_header"></Row>
-                    <Row>
-                        <Col className="phone_body_img">
-                            <Carousel>
-                                <Carousel.Item>
-                                    <img
-                                        className="phone_body_img_size"
-                                        src="https://www.instagram.com/static/images/homepage/screenshot1.jpg/d6bf0c928b5a.jpg"
-                                        alt="first"
-                                    />
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img
-                                        className="phone_body_img_size"
-                                        src="https://www.instagram.com/static/images/homepage/screenshot2.jpg/6f03eb85463c.jpg"
-                                        alt="second"
-                                    />
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img
-                                        className="phone_body_img_size"
-                                        src="https://www.instagram.com/static/images/homepage/screenshot3.jpg/f0c687aa6ec2.jpg"
-                                        alt="third"
-                                    />
-                                </Carousel.Item>
-                            </Carousel>
-                        </Col>
-                        
-                    </Row>
-                </Col>
+                        </Row>
+                    </Col>
 
-                <Col sm={{ span: 3 }} className="phone_body">
-                    <Row className="login_header_5"></Row>
-                    
-                    <Row className="justify-content-md-center container-margin-b10">
-                        <Card style={{ width: '22rem' }}>
-                            <Row className="login_header_3"></Row>
-                            <Row className="justify-content-md-center">
-                                <Col md="auto"><Card.Img variant="top" src="img/login/instargram_letter.png" /></Col>
+                    <Col sm={{ span: 3 }} className="phone_body text-center">
+                        <Container className="ligwidth">
+                            <Row className="login_header_5"></Row>
+
+                            <Row className="margin-b10 justify-content-center">
+                                <Card style={{width:'350px'}}>
+                                    <Row className="login_header_3"></Row>
+                                    
+                                    <Row className="justify-content-center">
+                                        <Col xs="auto" className="">
+                                            <Card.Img variant="top" src="img/login/instargram_letter.png" />
+                                        </Col>
+                                    </Row>
+                                    
+                                    <Row className="fluid">
+                                        <Card.Body></Card.Body>
+
+                                        <Col>
+                                            <Container>
+                                                <Row className="margin-b5">
+                                                    <InputGroup>
+                                                        <FormControl
+                                                          placeholder="E-Mail"
+                                                          aria-label="email"
+                                                          aria-describedby="basic-addon1"
+                                                        />
+                                                    </InputGroup>
+                                                </Row>
+
+                                                <Row className="margin-b10">
+                                                    <InputGroup>
+                                                        <FormControl
+                                                          placeholder="Password"
+                                                          aria-label="Password"
+                                                          aria-describedby="basic-addon1"
+                                                        />
+                                                    </InputGroup>
+                                                </Row>
+
+                                                <Row className="margin-b20">
+                                                    <div className="d-grid gap-2">
+                                                        <Button variant="primary" href="/Info">
+                                                        로그인
+                                                        </Button>
+                                                    </div>
+                                                </Row>
+
+                                                <Row className="margin-b10">
+                                                    <Col><p className="s311c"></p></Col>
+                                                    <Col><p className="text-center _0tv-g">또는</p></Col>
+                                                    <Col><p className="s311c"></p></Col>
+                                                </Row>
+
+                                                <Row className="justify-content-xs-center">
+                                                    <Col className="text-center"><Button variant="link">비번 잊어버리는 멍청이?</Button></Col>
+                                                </Row>
+                                            </Container>
+                                        </Col>
+
+                                        <Card.Body></Card.Body>
+                                    </Row>
+                                </Card>
                             </Row>
-                            <Row className="justify-content-md-center fluid">
-                                <Card.Body></Card.Body>
-                                
-                                <Col>
-                                    <Container>
-                                        <Row className="container-margin-b5">
-                                            <InputGroup>
-                                                <FormControl
-                                                  placeholder="E-Mail"
-                                                  aria-label="email"
-                                                  aria-describedby="basic-addon1"
-                                                />
-                                            </InputGroup>
+
+                            <Row className="margin-b10 justify-content-center">
+                                <Card style={{width:'350px'}}>
+                                    <Card.Body>
+                                        <Row className="justify-content-center">
+                                            <Col className="text-center">
+                                                SNS는 인생의 낭비다. <Link to="/SignUp"><span className="text-primary">가입</span></Link>
+                                            </Col>
                                         </Row>
-                                        
-                                        <Row className="container-margin-b10">
-                                            <InputGroup>
-                                                <FormControl
-                                                  placeholder="Password"
-                                                  aria-label="Password"
-                                                  aria-describedby="basic-addon1"
-                                                />
-                                            </InputGroup>
-                                        </Row>
-                                        
-                                        <Row className="container-margin-b20">
-                                            <div className="d-grid gap-2">
-                                                <Button variant="primary" href="/Info">
-                                                로그인
-                                                </Button>
-                                            </div>
-                                        </Row>
-                                        
-                                        <Row className="container-margin-b10">
-                                            <Col><p className="s311c"></p></Col>
-                                            <Col><p className="text-center _0tv-g">또는</p></Col>
-                                            <Col><p className="s311c"></p></Col>
-                                        </Row>
-                                        
-                                        <Row className="justify-content-md-center">
-                                            <Col className="text-center"><Button variant="link">비번 잊어버리는 멍청이?</Button></Col>
-                                        </Row>
-                                    </Container>
+                                    </Card.Body>
+                                </Card>
+                            </Row>
+
+                            <Row className="">
+                                <Col className="text-center">
+                                    <Row className="container-margin-t5">
+                                        <p>다운로드 앱 (미지원)</p>
+                                    </Row>
                                 </Col>
-                                
-                                <Card.Body></Card.Body>
                             </Row>
-                        </Card>
-                    </Row>
-                    
-                    <Row className="justify-content-md-center container-margin-b10">
-                        <Card style={{ width: '22rem' }}>
-                            <Card.Body>
-                                <Row className="justify-content-md-center">
-                                    <Col className="text-center">
-                                        SNS는 인생의 낭비다. <Link to="/SignUp"><span className="text-primary">가입</span></Link>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                        </Card>
-                    </Row>
-                    
-                    <Row className="justify-content-md-center">
-                        <Col className="text-center">
-                            <Row className="container-margin-t5">
-                                <p>다운로드 앱 (미지원)</p>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
+
+            
         </Container>
     );
 }
