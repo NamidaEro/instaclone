86345a1b842924bad1eef266951619fb49290b9a
진욱 <rkseksgkrns@kakao.com>
2021-08-29 13:09:28 +0000

    delete mongoose

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -25,27 +25,6 @@ router.use(session({
 router.use(passport.initialize());
 router.use(passport.session());
 
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://admin:1q2w3e4r@clusterzero.ucszf.mongodb.net/cinstagram?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// mongoose.connect(uri, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-// });
-
-// const db = mongoose.connection;
-// db.on("error", (err) => console.error(err));
-// db.once("open", () => console.log("데이터베이스 연결 성공"));
-
-// MongoClient.connect(uri)
-// .then(client => {
-//     console.log('mongo connected');
-//     let collection = client.db('cinstagram').collection('users');
-//     console.log(collection);
-// })
-// .catch(console.log);
-
 const sql = require('./sqlconnection.js');
 
 /* GET home page. */
